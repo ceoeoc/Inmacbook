ce616c5a243beeb7f718e30da541d3957572941f
ceoeoc <34293231+ceoeoc@users.noreply.github.com>
2018-04-11 23:37:26 +0900

    Second Commit add Tab4

---
+++ app/src/main/java/com/example/admin/practice/Tab4Fragment.java
@@ -0,0 +1,20 @@
+package com.example.admin.practice;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by qkrrj on 2018-04-11.
+ */
+
+public class Tab4Fragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.tab2_fragment, container, false);
+        return rootView;
+    }
+}
